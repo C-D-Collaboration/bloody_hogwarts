601643f91f7412aff5625610030543c1f16bb6cc
Dani Coleman <danicoleman00@gmail.com>
2021-03-18 16:04:24 -0500

    Added a within block to the student index test
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -7,11 +7,9 @@
 # -Age
 # -House
 # (e.g. "Name: Casseopia Black, Age: 14, House: Slytherin")
-
 require 'rails_helper'
 
 RSpec.describe 'student index' do
-
   before :each do
     @student1 = Student.create(name: "Harry", age: 12, house: "Griffindor")
     @student2 = Student.create(name: "Ron", age: 11, house: "Slytherin")
@@ -20,21 +18,27 @@ RSpec.describe 'student index' do
   end
 
   it 'can see a list of students with name, age, and house' do
-    visit "/students"
+    visit students_path
 
-    expect(page).to have_content(@student1.name)
-    expect(page).to have_content(@student2.name)
-    expect(page).to have_content(@student3.name)
-    expect(page).to have_content(@student4.name)
-
-    expect(page).to have_content(@student1.age)
-    expect(page).to have_content(@student2.age)
-    expect(page).to have_content(@student3.age)
-    expect(page).to have_content(@student4.age)
-
-    expect(page).to have_content(@student1.house)
-    expect(page).to have_content(@student2.house)
-    expect(page).to have_content(@student3.house)
-    expect(page).to have_content(@student4.house)
+    expect(page).to have_content('All Students')
+    within(first('.student')) do
+      expect(page).to have_css('.name')
+      expect(page).to have_css('.age')
+      expect(page).to have_css('.house')
+    end
+    # expect(page).to have_content(@student1.name)
+    # expect(page).to have_content(@student2.name)
+    # expect(page).to have_content(@student3.name)
+    # expect(page).to have_content(@student4.name)
+    #
+    # expect(page).to have_content(@student1.age)
+    # expect(page).to have_content(@student2.age)
+    # expect(page).to have_content(@student3.age)
+    # expect(page).to have_content(@student4.age)
+    #
+    # expect(page).to have_content(@student1.house)
+    # expect(page).to have_content(@student2.house)
+    # expect(page).to have_content(@student3.house)
+    # expect(page).to have_content(@student4.house)
   end
 end
