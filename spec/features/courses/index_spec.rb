e2e128eda27b4198c1cfa10134777e16b871f68f
Dani Coleman <danicoleman00@gmail.com>
2021-03-22 16:40:18 -0500

    Add test to show students per course in alphabetical order
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- spec/features/courses/index_spec.rb
+++ spec/features/courses/index_spec.rb
@@ -9,19 +9,30 @@ RSpec.describe 'Courses', type: :feature do
       @student4 = Student.create(name: "Beth", age: 13, house: "Ravenclaw")
 
       @course1 = Course.create(name: "Defense against the Dark Arts")
+      # Ron, Jenny
       @course3 = Course.create(name: "Potions")
+      # Harry, Ron, Beth
       @course2 = Course.create(name: "Herbology")
+      # Harry, Jenny, Beth
 
       StudentCourse.create(student_id: @student1.id, course_id: @course2.id)
       StudentCourse.create(student_id: @student1.id, course_id: @course3.id)
+
       StudentCourse.create(student_id: @student2.id, course_id: @course3.id)
+      StudentCourse.create(student_id: @student2.id, course_id: @course1.id)
+
+      StudentCourse.create(student_id: @student3.id, course_id: @course1.id)
+      StudentCourse.create(student_id: @student3.id, course_id: @course2.id)
+
+      StudentCourse.create(student_id: @student4.id, course_id: @course3.id)
+      StudentCourse.create(student_id: @student4.id, course_id: @course2.id)
     end
 
     it 'can see a list of courses and the number of students enrolled per course' do
       visit courses_path
 
       expect(page).to have_content("#{@course3.name}: #{@course3.students.count}")
-      expect(page).to_not have_content("#{@course3.name}: #{@course2.students.count}")
+      # expect(page).to_not have_content("#{@course3.name}: #{@course2.students.count}")
     end
 
     it "shows courses in alphabetical order" do
@@ -31,6 +42,14 @@ RSpec.describe 'Courses', type: :feature do
       expect(@course2.name).to appear_before(@course3.name)
 
       expect(@course2.name).to_not appear_before(@course1.name)
+    end
+
+    it "show student per course in alphabetical order" do
+      visit courses_path
+
+      expect(@course1.students).to eq([@student3, @student2])
+      expect(@course2.students).to eq([@student4, @student1, @student3])
+      expect(@course3.students).to eq([@student4, @student1, @student2])
     end
   end
 end
