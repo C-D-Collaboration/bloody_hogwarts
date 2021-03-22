25436634e794235e1a014dd4010d21246c25aab2
Dani Coleman <danicoleman00@gmail.com>
2021-03-22 16:03:03 -0500

    Add model method sorted_courses to sort courses by name and test
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,10 @@ class Course < ApplicationRecord
 
   has_many :student_courses
   has_many :students, through: :student_courses
+
+  def self.sorted_courses(all_courses)
+    all_courses.sort_by do |course|
+      course.name
+    end
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Course, type: :model do
     it {should have_many(:students).through(:student_courses)}
   end
 
+  describe 'instance methods' do
+    it "can sort courses by name" do
+      course1 = Course.create(name: "Defense against the Dark Arts")
+      course3 = Course.create(name: "Potions")
+      course2 = Course.create(name: "Herbology")
+
+      all_courses = Course.all
+      sorted = Course.sorted_courses(all_courses)
+
+      expect(sorted).to eq([course1, course2, course3])
+    end
+  end
 end
