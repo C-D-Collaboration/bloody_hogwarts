102e1e0c0690ecd26dc75970f1b6fc6302c613fc
Dani Coleman <danicoleman00@gmail.com>
2021-03-22 16:46:53 -0500

    WIP calling a created method for all students in each course alphabetized
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,9 @@
 class CoursesController < ApplicationController
   def index
+    all_students = Student.all
     all_courses = Course.all
     @courses = Course.sorted_courses(all_courses)
+    @courses_students = Course.sorted_students(all_students)
+    require "pry"; binding.pry
   end
 end
