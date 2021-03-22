8b0df34b9501ca40db7711cdb9ab28cb3fb97fcb
Dani Coleman <danicoleman00@gmail.com>
2021-03-22 16:49:03 -0500

    WIP method to get all student in a course alphabetized
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,4 +9,13 @@ class Course < ApplicationRecord
       course.name
     end
   end
+
+  def self.sorted_students(all_courses)
+    all_courses.each do |course|
+      course.students.sort_by do |student|
+        student.name
+        # require "pry"; binding.pry
+      end
+    end
+  end
 end
